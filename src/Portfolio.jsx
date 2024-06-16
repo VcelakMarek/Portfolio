4f52ba0f55e0995cf3b4b96e5ef1e5ec0f620c31
VcelakMarek <vcelak.marek@gmail.com>
2024-06-16 17:52:34 +0200

    update Portfolio

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -48,8 +48,12 @@ const Portfolio = () => {
           <p className="name">
             Hi, my name is <span>Marek Vcelak</span>
           </p>
-          <h2>React Developer</h2>
-          <p>I am frontend developer specializing on React aplications</p>
+          <h2>Frontend Developer</h2>
+          <p>
+            I am frontend developer specializing on
+            <span className="green-text"> React </span> and{" "}
+            <span className="green-text">Next.js</span> aplications
+          </p>
         </section>
       </div>
 
@@ -64,10 +68,13 @@ const Portfolio = () => {
               <h4>First Next.js project</h4>
               <h3>Book Collection</h3>
               <p className="blackbox">
-                The Book Collection displays books from an external API and
-                includes an admin section (currently without authorization)
-                enabling book editing, deletion, and addition. You can see
-                source code{" "}
+                The Book Collection is my Next.js project, developed as part of
+                an interview assignment. It retrieves book data from an external
+                API and features an admin section (currenty without
+                authorization) for editing, deleting and adding books. This
+                project provided me with the chance to explore Next.js and
+                enhance my proficiency in web development.
+                <br /> You can see source code{" "}
                 <a href="https://github.com/VcelakMarek/Book_collection">
                   here
                 </a>{" "}
@@ -76,7 +83,7 @@ const Portfolio = () => {
               </p>
               <h4>Technologies used include:</h4>
               <ul>
-                <li>Next js</li>
+                <li>Next.js</li>
                 <li>Typescript</li>
                 <li>Tailwind</li>
                 <li>React final form</li>
@@ -98,24 +105,67 @@ const Portfolio = () => {
         <section id="projects">
           <h2>React projects</h2>
 
+          <article className="reverse">
+            <div className="text">
+              <h4>Interview project</h4>
+              <h3>GitHub Finder</h3>
+              <p className="blackbox">
+                The GitHub Finder is a responsive web application I developed as
+                part of an interview project. This app allows users to search
+                for GitHub profiles and view their repositories. While building
+                it, I had the opportunity to work with some new technologies
+                like React Query and MUI. It is written in React using
+                TypeScript and includes error handling to ensure a smooth user
+                experience. <br /> You can see source code{" "}
+                <a href="https://github.com/VcelakMarek/Interview_Project">
+                  here
+                </a>{" "}
+                and application{" "}
+                <a href="https://vcelakmarek.github.io/Interview_Project/">
+                  {" "}
+                  here.
+                </a>
+              </p>
+              <h4>Technologies used include:</h4>
+              <ul>
+                <li>React</li>
+                <li>MUI</li>
+                <li>React Query</li>
+                <li>Typescript</li>
+                <li>Axios</li>
+                <li>Vite</li>
+                <li>Prettier</li>
+              </ul>
+            </div>
+            <img
+              src="./github-finder.jpg"
+              alt="Screenshot of IP_Address_Tracker."
+            />
+          </article>
+
           <article>
             <div className="text">
               <h4>Current Project</h4>
               <h3>Invoice app</h3>
               <p className="blackbox">
-                This application is for managing invoices which can be created,
-                edited, deleted or you can change their status. Form use
-                validation when new invoice is created. You can also filter
-                these invoices by status. Any changes are saved into
-                localStorage so you can&apos;t lose your data. This is my
-                current project and I still work on it. You can see source code{" "}
-                <a href="https://github.com/VcelakMarek/Invoice_app">here</a>{" "}
+                The Invoice App is designed to help you manage your invoices
+                efficiently. With this application, you can create, edit,
+                delete, and update the status of your invoices. The form
+                includes validation to ensure accuracy when creating new
+                invoices. You can also filter invoices by status for easy
+                organization. All changes are saved to localStorage, ensuring
+                that your data remains available even after closing the browser.
+                <br />
+                You can see source code{" "}
+                <a href="https://github.com/VcelakMarek/Invoice_app">
+                  here
+                </a>{" "}
                 and application{" "}
                 <a href="https://vcelakmarek.github.io/Invoice_app/"> here.</a>
               </p>
               <h4>Technologies used include:</h4>
               <ul>
-                <li>React js</li>
+                <li>React</li>
                 <li>Typescript</li>
                 <li>Tailwind</li>
                 <li>React final form</li>
@@ -131,10 +181,12 @@ const Portfolio = () => {
               <h4>Maps Project</h4>
               <h3>IP Address Tracker</h3>
               <p className="blackbox">
-                This application displays informations about entered IP adress
-                and show location on map. After opening this application it will
-                be displayed informatins about your IP address. You can see
-                source code{" "}
+                The IP Address Tracker is a web-based tool designed to provide
+                users with information about entered IP addresses. Upon opening
+                the application, users are greeted with details about their own
+                IP address. They can then enter any IP address of interest to
+                retrieve information such as geographical location and map
+                coordinates.{" "}
                 <a href="https://github.com/VcelakMarek/IP_Address_Tracker">
                   here
                 </a>{" "}
@@ -146,7 +198,7 @@ const Portfolio = () => {
               </p>
               <h4>Technologies used include:</h4>
               <ul>
-                <li>React js</li>
+                <li>React</li>
                 <li>SASS</li>
                 <li>Ipifi API</li>
                 <li>Leaflet maps</li>
@@ -165,10 +217,11 @@ const Portfolio = () => {
               <h4>Data fetch project</h4>
               <h3>GitHub user search app</h3>
               <p className="blackbox">
-                This aplication display details about any user on GitHub. I
-                choose this because I want to learn basics of async function and
-                data fetching. This project also includes light/dark mode
-                switch. You can see source code{" "}
+                This application provides users with detailed information about
+                any GitHub user. Primary goal of this project is to learn the
+                basics of asynchronous functions and data fetching.
+                Additionally, it features a light/dark mode switch. <br /> You
+                can see source code{" "}
                 <a href="https://github.com/VcelakMarek/GitHub_user_search_app">
                   here
                 </a>{" "}
@@ -180,7 +233,7 @@ const Portfolio = () => {
               </p>
               <h4>Technologies used include:</h4>
               <ul>
-                <li>React js</li>
+                <li>React</li>
                 <li>API</li>
                 <li>Vite</li>
                 <li>Prettier</li>
@@ -197,16 +250,18 @@ const Portfolio = () => {
               <h4>First React project</h4>
               <h3>Tic Tac Toe</h3>
               <p className="blackbox">
-                This was my first React project. You can choose mark and play
-                with another player or play agains CPU. Aplication counts each
-                player wins. You can see source code{" "}
+                The Tic Tac Toe application offers players the flexibility to
+                choose between playing against another player or challenging the
+                CPU. Additionally, players have the option to select their
+                preferred mark (X or O). The application also tracks the
+                winner's score. <br /> You can see source code{" "}
                 <a href="https://github.com/VcelakMarek/Tic_Tac_Toe">here</a>{" "}
                 and application{" "}
                 <a href="https://vcelakmarek.github.io/Tic_Tac_Toe/"> here.</a>
               </p>
               <h4>Technologies used include:</h4>
               <ul>
-                <li>React js</li>
+                <li>React</li>
                 <li>CSS</li>
                 <li>Vite</li>
                 <li>Prettier</li>
@@ -230,8 +285,12 @@ const Portfolio = () => {
               <h4>Another JS project</h4>
               <h3>Password generator</h3>
               <p className="blackbox">
-                This application create a password according to the selected
-                options and length. You can see source code{" "}
+                This application is designed to generate passwords based on
+                user-selected options and desired length. One of the unique
+                features of this project is the dynamic sizing of the password
+                font, ensuring that the generated password remains readable and
+                does not exceed the available display area. <br />
+                You can see source code{" "}
                 <a href="https://github.com/VcelakMarek/Password_generator">
                   here
                 </a>{" "}
@@ -259,7 +318,8 @@ const Portfolio = () => {
               <h4>First JS project</h4>
               <h3>Calculator</h3>
               <p className="blackbox">
-                This is just normal Calculator. You can see source code{" "}
+                This simple calculator marks the beginning of my JavaScript
+                learning journey. <br /> You can see source code{" "}
                 <a href="https://github.com/VcelakMarek/Calculator">here</a> and
                 application{" "}
                 <a href="https://vcelakmarek.github.io/Calculator//"> here.</a>
@@ -279,7 +339,10 @@ const Portfolio = () => {
               <h4>First CSS project</h4>
               <h3>Pricing component</h3>
               <p className="blackbox">
-                This was my first CSS project. You can see source code{" "}
+                This project serves as my introduction to CSS basics, showcasing
+                responsive design skills. It's a simple pricing component
+                allowing users to switch between annual and monthly options.{" "}
+                <br /> You can see source code{" "}
                 <a href="https://github.com/VcelakMarek/First_Project">here</a>{" "}
                 and application{" "}
                 <a href="https://vcelakmarek.github.io/First_Project/">
@@ -307,7 +370,12 @@ const Portfolio = () => {
       <div className="contact-background">
         <section id="contact">
           <h2>Contact me</h2>
-          <p>I am interested about new job opportunities.</p>
+          <p>
+            I'm available for new job opportunities in frontend development, and
+            I'm particularly interested in roles involving React and Next.js.
+            Please don't hesitate to contact me if you have any relevant
+            openings.
+          </p>
           <a href="mailto:vcelak.marek@gmail.com">Email me</a>
         </section>
       </div>
@@ -340,7 +408,7 @@ const Portfolio = () => {
           </li>
         </ul>
         <p>
-          <small> &copy; 2023 Marek Vcelak. All rights reserved. </small>
+          <small> &copy; 2024 Marek Vcelak. All rights reserved. </small>
         </p>
       </footer>
     </>
